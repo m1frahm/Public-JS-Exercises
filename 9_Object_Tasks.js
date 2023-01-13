b183baca1d297a1056ae6a05a366819a582a3ebb
Mifrah Malik <mifrah.malik@gmail.com>
2023-01-13 11:36:42 -0500

    new assignment

---
+++ 9_Object_Tasks.js
@@ -0,0 +1,6 @@
+//https://javascript.info/object#tasks
+//example 
+// let user = {     // an object
+//   name: "John",  // by key "name" store value "John"
+//   age: 30        // by key "age" store value 30
+// };
